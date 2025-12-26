5b7726374d04ba32130c9ec4d7c961b97972e9b5
Ivanova Viktoria <ivanovaviktoria97@yandex.by>
2025-12-26 21:01:02 +0300

    add: persist to store

--- coincap/src/RTK/store.js
+++ coincap/src/RTK/store.js
@@ -1,12 +1,31 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
 import assetsReducer from './slices/assetsSlice'
 import portfolioReducer from './slices/portfolioSlice'
 
+const assetsPersist = {
+  key: 'assets',
+  storage,
+  whitelist: ['assets'],
+}
+
+const currencyPersist = {
+  key: 'currency',
+  storage,
+  whitelist: ['currency'],
+}
+
 const rootReducer = combineReducers({
-  assets: assetsReducer,
-  currency: portfolioReducer,
+  assets: persistReducer(assetsPersist, assetsReducer),
+  currency: persistReducer(currencyPersist, portfolioReducer),
 })
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 })
+export const persistor = persistStore(store)
